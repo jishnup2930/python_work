e3450a1b09252f5d98221bbe0a7efe1605f6fc88
jishnu <givemeakiss111@gmail.com>
2023-10-31 17:09:22 +0530

    Implemented unit test for function freq
    Tested the freq function with assertion

--- program1.py
+++ program1.py
@@ -1,5 +1,7 @@
 import unittest
 from program import palindrome
+from program import freq
+
 class TestPalindrome(unittest.TestCase):
     def testIsPalindrome(self):
         sentence = "abba"
@@ -16,6 +18,24 @@ class TestPalindrome(unittest.TestCase):
     def testSpecialcharactersInput(self):
         special_char='!@#$%^&.\;/<>,*'
         self.assertFalse(palindrome(special_char))
+
+
+class TestFrequency(unittest.TestCase):
+    def testIsFreq(self):
+        sentence = "jishnu"
+        self.assertTrue(freq(sentence))
+    
+    def testNullInput(self):
+        ret = freq("")
+        self.assertFalse(ret)
+
+    def testSpecialcharactersInput(self):
+        special_char='!@#$%^&.\;/<>,*'
+        self.assertTrue(freq(special_char))
+
+    def testNumberInput(self):
+        numbers='1234567890'
+        self.assertTrue(freq(numbers))
 
 
 
