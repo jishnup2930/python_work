3a945a14114b913656ea3d12db98ca317f270fd2
jishnu <givemeakiss111@gmail.com>
2023-11-01 15:11:20 +0530

    Added the output values in thetesting code

--- program1.py
+++ program1.py
@@ -1,28 +1,28 @@
 import unittest
-from program import palindrome
-from program import freq
+from program import palindrome,freq
 
 class TestPalindrome(unittest.TestCase):
-    def testIsPalindrome(self):
+    def test_IsPalindrome(self):
         sentence = "abba"
         self.assertTrue(palindrome(sentence))
 
-    def testIsNotPalindrome(self):
+    def test_IsNotPalindrome(self):
         sentence = "not palindrome"
         self.assertFalse(palindrome(sentence))
     
-    def testNullInput(self):
+    def test_NullInput(self):
         ret = palindrome("")
         self.assertTrue(ret)
 
-    def testSpecialcharactersInput(self):
+    def test_SpecialcharactersInput(self):
         special_char='!@#$%^&.\;/<>,*'
         self.assertFalse(palindrome(special_char))
 
 
 class TestFrequency(unittest.TestCase):
     def testIsFreq(self):
-        sentence = "jishnu"
+        sentence = "jishnu ji"
+        d={'j':2,'i':2,'s':1,'h':1,'n':1,'u':1}
         self.assertTrue(freq(sentence))
     
     def testNullInput(self):
@@ -30,11 +30,13 @@ class TestFrequency(unittest.TestCase):
         self.assertFalse(ret)
 
     def testSpecialcharactersInput(self):
-        special_char='!@#$%^&.\;/<>,*'
+        special_char='!@#'
+        d={'!':1,'@':1,'#':1}
         self.assertTrue(freq(special_char))
 
     def testNumberInput(self):
         numbers='1234567890'
+        d={1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,0:1}
         self.assertTrue(freq(numbers))
 
 
