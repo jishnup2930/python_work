4f4f7553077736ef7eb429c7ac72e3353c6a7d43
jishnu <givemeakiss111@gmail.com>
2023-11-01 17:52:20 +0530

    Code rearranged to pass the two operand test

--- postfix.py
+++ postfix.py
@@ -1,2 +1,11 @@
 def evaluate(exp):
-    return exp
+    stack = []
+    for i in exp:
+        if i == '+':
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            stack.append(operand1+operand2)
+        else:
+            stack.append(int(i))
+    return stack.pop()
+
