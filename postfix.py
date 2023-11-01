7e76873f93b520a3e07b1f6f47fea9a85c91041b
jishnu <givemeakiss111@gmail.com>
2023-11-01 17:59:47 +0530

    Added new condition to sustract

--- postfix.py
+++ postfix.py
@@ -5,6 +5,8 @@ def evaluate(exp):
             operand1 = stack.pop()
             operand2 = stack.pop()
             stack.append(operand1+operand2)
+        elif i == '-':
+            stack.append(operand1-operand2)
         else:
             stack.append(int(i))
     return stack.pop()
